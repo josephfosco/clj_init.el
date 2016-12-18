c12396638836c85e7ddacfce924c6a899bb701c9
Joseph Fosco <joseph@josephfosco.com>
2016-12-17 18:20:33 -0600

    dont load cider

--- init.el
+++ init.el
@@ -35,11 +35,11 @@
   (push-mark)
   (goto-char (point-min)))
 
-(live-add-pack-lib "ac-cider")
-(require 'ac-cider)
-(add-hook 'cider-mode-hook 'ac-flyspell-workaround)
-(add-hook 'cider-mode-hook 'ac-cider-setup)
-(add-hook 'cider-repl-mode-hook 'ac-cider-setup)
+;; (live-add-pack-lib "ac-cider")
+;; (require 'ac-cider)
+;; (add-hook 'cider-mode-hook 'ac-flyspell-workaround)
+;; (add-hook 'cider-mode-hook 'ac-cider-setup)
+;; (add-hook 'cider-repl-mode-hook 'ac-cider-setup)
 (eval-after-load "auto-complete"
   '(progn
      (add-to-list 'ac-modes 'cider-mode)
